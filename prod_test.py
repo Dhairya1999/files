ee7444f686564a5ce8e0565e2ab1b06d2f423812
Dash_Cosmatos77 <53261974+Dhairya1999@users.noreply.github.com>
2022-07-28 22:16:36 -0500

    Update prod_test.py

--- prod_test.py
+++ prod_test.py
@@ -1,14 +1,23 @@
 import tweepy
+from transformers import pipeline
+import json
+import configparser
 
-streaming_client = tweepy.StreamingClient("AAAAAAAAAAAAAAAAAAAAAC07fQEAAAAAB7CyzHT8s1HfYB3lrrKa4rPx5P8%3DNnYqfBabqoI4nkGXAvIIgnsRa6JTtN89mnkdsaePieVAc9T5qy")
-streaming_client.add_rules(tweepy.StreamRule("Covid-19"))
-streaming_client.filter()
+def perform_analysis(text):
+	transformer_sentiment = classifier(text)
+	transformer_sentiment = transformer_sentiment[0]['label']
+	return transformer_sentiment
 
 class IDPrinter(tweepy.StreamingClient):
+    def _init_(self):
+        self.producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 
     def on_tweet(self, tweet):
-        print(tweet.id)
-
-
-printer = IDPrinter("AAAAAAAAAAAAAAAAAAAAAC07fQEAAAAAB7CyzHT8s1HfYB3lrrKa4rPx5P8%3DNnYqfBabqoI4nkGXAvIIgnsRa6JTtN89mnkdsaePieVAc9T5qy")
-printer.sample()
+        a=perform_analysis(tweet.text)
+        self.producer.send("tp1", a.encode('utf-8'))
+        
+        
+classifier = pipeline('sentiment-analysis')
+printers = IDPrinter("AAAAAAAAAAAAAAAAAAAAAC07fQEAAAAAB7CyzHT8s1HfYB3lrrKa4rPx5P8%3DNnYqfBabqoI4nkGXAvIIgnsRa6JTtN89mnkdsaePieVAc9T5qy")
+printers.add_rules(tweepy.StreamRule("Covid-19"))
+printers.filter()
