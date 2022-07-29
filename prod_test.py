e74341eb2654eda725895fc960a7622edecdcedf
Dash_Cosmatos77 <53261974+Dhairya1999@users.noreply.github.com>
2022-07-28 22:03:19 -0500

    Update prod_test.py

--- prod_test.py
+++ prod_test.py
@@ -1,6 +1,6 @@
 import tweepy
 
-streaming_client = tweepy.StreamingClient("Bearer Token here")
+streaming_client = tweepy.StreamingClient("AAAAAAAAAAAAAAAAAAAAAC07fQEAAAAAB7CyzHT8s1HfYB3lrrKa4rPx5P8%3DNnYqfBabqoI4nkGXAvIIgnsRa6JTtN89mnkdsaePieVAc9T5qy")
 streaming_client.add_rules(tweepy.StreamRule("Covid-19"))
 streaming_client.filter()
 
@@ -10,5 +10,5 @@ class IDPrinter(tweepy.StreamingClient):
         print(tweet.id)
 
 
-printer = IDPrinter("Bearer Token here")
+printer = IDPrinter("AAAAAAAAAAAAAAAAAAAAAC07fQEAAAAAB7CyzHT8s1HfYB3lrrKa4rPx5P8%3DNnYqfBabqoI4nkGXAvIIgnsRa6JTtN89mnkdsaePieVAc9T5qy")
 printer.sample()
